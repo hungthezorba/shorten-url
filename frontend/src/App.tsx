6a1e6494cbb6b90d495e7a243a5ac555a6444d69
hungthezorba <hungthezorba@gmail.com>
2023-12-19 11:43:20 +0700

    feat: add driverjs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useLayoutEffect, useMemo, useState } from "react";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { createShortenUrl } from "./apis/create-url";
@@ -6,12 +6,19 @@ import { ClipboardIcon } from "@radix-ui/react-icons";
 import { useToast } from "./components/ui/use-toast";
 import { DefaultLayout } from "./layouts/default-layout";
 import { Loader2 } from "lucide-react";
+import { isValidHttpUrl } from "./utils/url";
+import { driver } from "driver.js";
+import "driver.js/dist/driver.css";
 
 function App() {
   const [url, setUrl] = useState("");
   const [shortenUrl, setShortenUrl] = useState("");
   const { toast } = useToast();
   const [loading, setLoading] = useState(false);
+
+  const isValidUrl = useMemo(() => {
+    return isValidHttpUrl(url);
+  }, [url]);
 
   const computedUrl = useMemo(() => {
     if (shortenUrl) return `${window.location.host}/${shortenUrl}`;
@@ -41,22 +48,82 @@ function App() {
     });
   };
 
+  const reset = () => {
+    setUrl("");
+    setShortenUrl("");
+  };
+
+  const driverObj = driver({
+    showProgress: true,
+    steps: [
+      {
+        element: "#url__input",
+        popover: {
+          title: "Input the url",
+          description: "Please place your url in here to shorten",
+          onNextClick: () => {
+            setUrl("https://github.com/hungthezorba/shorten-url");
+            driverObj.moveNext();
+          },
+        },
+      },
+      {
+        element: "#shorten-url__button",
+        popover: {
+          title: "Click the button",
+          side: "right",
+          description:
+            "Click Shorten URL button here and wait for the shortened link to appear",
+          onNextClick: async () => {
+            await handleShortenUrl();
+            driverObj.moveNext();
+          },
+        },
+      },
+      {
+        element: "#shortened-url__box",
+        popover: {
+          title: "Congratulation 🎉",
+          description: "Your shorten url is ready to used",
+          side: "bottom",
+          onCloseClick: () => {
+            reset();
+            driverObj.moveNext();
+          },
+          onNextClick: () => {
+            reset();
+            driverObj.moveNext();
+          },
+        },
+      },
+    ],
+  });
+
+  useLayoutEffect(() => {
+    driverObj.drive();
+  }, []);
+
   return (
     <DefaultLayout>
       <div className="flex justify-center mx-auto flex-col gap-4 w-full h-screen items-center">
         <div className="flex max-w-sm items-center justify-center space-x-2 w-full">
           <Input
+            id="url__input"
             onChange={handleChange}
             value={url}
             placeholder="Enter the link here"
           />
-          <Button disabled={!url} onClick={handleShortenUrl}>
+          <Button
+            id="shorten-url__button"
+            disabled={!isValidUrl}
+            onClick={handleShortenUrl}
+          >
             {loading && <Loader2 />}
             Shorten URL
           </Button>
         </div>
-        {computedUrl && (
-          <div className="flex flex-col gap-2">
+        <div id="shortened-url__box" className="flex flex-col gap-2">
+          {computedUrl && (
             <div
               onClick={handleCopy}
               className="hover:shadow-md drop-shadow-lg bg-secondary py-2 flex gap-2 px-4 rounded-sm justify-between items-center cursor-pointer"
@@ -64,8 +131,8 @@ function App() {
               <p>{computedUrl}</p>
               <ClipboardIcon />
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </DefaultLayout>
   );
